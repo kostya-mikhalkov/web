c8247c84c09879e6814306c034aa1a0a7e46de68
Kostya <kostya.mikhalkov@bk.ru>
2022-07-02 17:21:58 +0300

    third

--- src/js/script.js
+++ src/js/script.js
@@ -13,4 +13,106 @@ $(document).ready(function(){
             }
         ]
       });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+          })
+      };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+      // Modal
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+      })
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+      })
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn();
+        })
+      })
+
+      // $('#consultation-form').validate();
+      // $('#consultation form').validate({
+      //     rules: {
+      //       name: "required",
+      //       phone: "required",
+      //       email: {
+      //         required: true,
+      //         email: true
+      //       }
+      //     },
+      //     messages: {
+      //       name: "Введите ваше имя",
+      //       phone: "Введите свой номер телефона",
+      //       email: {
+      //         required: "Введите свой почтовый адрес",
+      //         email: "Неправильно введен адрес почты"
+      //       }
+      //     }
+      // });
+      // $('#order form').validate();
+
+      function valideForms(form){
+        $(form).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "Введите ваше имя",
+            phone: "Введите свой номер телефона",
+            email: {
+              required: "Введите свой почтовый адрес",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+      });
+      };
+      valideForms('#consultation-form');
+      valideForms('#consultation form');
+      valideForms('#order form');
+
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+        
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+
+
+          $('form').trigger('reset');
+        });
+        return false;
+      });
   });
+    
